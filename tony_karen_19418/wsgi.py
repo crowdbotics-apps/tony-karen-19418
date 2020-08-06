36234e98fb21eda7ec35ea9e3a465081a823d372
Crowdbotics <team@crowdbotics.com>
2020-08-06 08:44:58 +0000

    Initial commit.

---
+++ tony_karen_19418/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tony_karen_19418 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tony_karen_19418.settings')
+
+application = get_wsgi_application()
